e3eabbd3948a61e8240c5bd9925720370baa25b8
soda-macbook <69882714+adelina-lee@users.noreply.github.com>
2022-09-19 09:01:13 +0800

    Basic DOM traversing

--- js/ch9/dom.js
+++ js/ch9/dom.js
@@ -1,3 +1,11 @@
 function reveal(el) {
     console.log(el);
 }
+
+// console.dir(document.body);
+
+// This will display the treasure 
+console.dir(document.body.children.forest.children.tree2.children.shruberry.children.treasure);
+
+//
+console.dir(document.body.childNodes[3].childNodes[3].childNodes[1].childNodes[1]);
