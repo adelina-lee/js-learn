02161822f7592a00e5783a0ab8b3babb0234845c
soda-macbook <69882714+adelina-lee@users.noreply.github.com>
2023-01-02 15:14:39 +0800

    Completed Chapter 9, on Chapter 10

--- js/ch9/dom.js
+++ js/ch9/dom.js
@@ -7,5 +7,7 @@ function reveal(el) {
 // This will display the treasure 
 console.dir(document.body.children.forest.children.tree2.children.shruberry.children.treasure);
 
-//
 console.dir(document.body.childNodes[3].childNodes[3].childNodes[1].childNodes[1]);
+
+// Shorter version of the above
+console.dir(document.body.childNodes[3].childNodes[3].childNodes[1].children.treasure);
